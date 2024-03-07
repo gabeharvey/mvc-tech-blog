40008a7bfb429db5199720a1e38a49a5e3ec093f
Gabe Harvey <harvey7@gmail.com>
2024-03-07 12:27:54 -0600

    Established Model Relationships in index.js

--- models/index.js
+++ models/index.js
@@ -2,3 +2,39 @@
 const Comment = require("./comment");
 const Post = require("./post");
 const User = require("./user");
+
+// Establishes One to Many Relationship Between User and Post Models
+// Each User Can Have Multiple Posts
+User.hasMany(Post, {
+    foreignKey: "user_id",
+});
+
+// Establishes Each Post Belongs to One User
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+// Establishes Each Comment Belongs to One User
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+// Establishes Each Comment Belongs to One Post
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+});
+
+// Establishes One to Many Relationship Between Post and Comment Models
+// Each Post Can Have Multiple Comments
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+});
+
+// Establishes One to Many Relationship Between User and Comment Models
+// Each User Can Have Multiple Comments
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+});
+
+// This Exports User, Post, and Comment Models
+module.exports = { User, Post, Comment };
