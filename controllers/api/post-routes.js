2b415fae89be8c277ba7995cb77de6d426500cf4
Gabe Harvey <harvey7@gmail.com>
2024-03-08 14:37:30 -0600

    Authenticated Post Function Added to controllers/api/post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -33,3 +33,16 @@ router.get("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// Create New Post Function when User Authenticated
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
