650d2adcd3bed36efc6635c337e6c98ea54fe0fd
Gabe Harvey <harvey7@gmail.com>
2024-03-08 12:55:06 -0600

    Added Required Packages to controllers/api/index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,5 @@
+// Packages Required for Application
+const commentRoutes = require("./comment-routes");
+const postRoutes = require("./post-routes");
+const userRoutes = require("./user-routes");
+const router = require("express").Router();
