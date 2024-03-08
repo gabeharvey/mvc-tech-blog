aabbbfa6d8421da69462fab707ca2929647affd7
Gabe Harvey <harvey7@gmail.com>
2024-03-08 15:55:19 -0600

    Added New User Sign Up Function to controllers/api/user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,3 +13,22 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// Function Allows New User Sign Up
+router.post("/signup", async (req, res) => {
+    try {
+        const newUser = new User();
+        newUser.username = req.body.username;
+        newUser.email = req.body.email;
+        newUser.password = req.body.password;
+        const userData = await newUser.save();
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+        console.log(err);
+    }
+});
