6861a4d30b1662805c450005ff7a7ce9bba7277a
Gabe Harvey <harvey7@gmail.com>
2024-03-08 13:01:38 -0600

    Established Required Routes in controllers/api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,3 +3,11 @@ const commentRoutes = require("./comment-routes");
 const postRoutes = require("./post-routes");
 const userRoutes = require("./user-routes");
 const router = require("express").Router();
+
+// This Establishes Routes Required for Application
+router.use("/comments", commentRoutes);
+router.use("/posts", postRoutes);
+router.use("/users", userRoutes);
+
+// This Exports Router
+module.exports = router;
