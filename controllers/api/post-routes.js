ec617366ce799d76c564f744d23812148acf9c57
Gabe Harvey <harvey7@gmail.com>
2024-03-08 14:00:17 -0600

    Added Function that Retrieves User Post by ID in controllers/api/post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,12 +3,31 @@ const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 const router = require("express").Router();
 
-// Retrieves Posts by User
+// Retrieves All Posts by User
 router.get("/", async (req, res) => {
     try {
         const postData = await Post.findAll({
             include: [{ model: User, attributes: ["username"] }],
         });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// This Retrieves User Post by ID
+router.get("/:id", async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                { model: User, attributes: ["username"] },
+                { model: Comment, include: [{ model: User, attributes: ["username"] }],},
+            ],
+        });
+        if (!postData) {
+            res.status(404).json({ message: "No Post Found." });
+            return;
+        }
         res.status(200).json(postData);
     } catch (err) {
         res.status(500).json(err);
