f66e470cd472f0e709cda9c7416b566b2e7c43b2
Gabe Harvey <harvey7@gmail.com>
2024-03-08 13:43:48 -0600

    Added Function to Retrieve Posts by User in controllers/api/post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,3 +2,15 @@
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 const router = require("express").Router();
+
+// Retrieves Posts by User
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{ model: User, attributes: ["username"] }],
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
