71e6810b1aac64c1cb7e8b806e9a0ebeb853e39c
Gabe Harvey <harvey7@gmail.com>
2024-03-08 12:48:51 -0600

    Established Routes for Application in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,10 @@
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 const router = require("express").Router();
+
+// This Establishes Routes for Application
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+
+// This Exports Router
+module.exports = router;
