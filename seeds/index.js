40ae8ed62b31ed2e1f36870f3796cd169eeebd6d
Gabe Harvey <harvey7@gmail.com>
2024-03-09 13:16:33 -0600

    Added Function to Seed Database in seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -2,3 +2,16 @@
 const seedComments = require("./commentData");
 const seedPosts = require("./postData");
 const seedUsers = require("./userData");
+const sequelize = require("../config/connection");
+
+// This Function Seeds Database
+const seedAll = async () => {
+    await sequelize.sync ({ force: true });
+    await seedComments();
+    await seedPosts();
+    await seedUsers();
+    process.exit(0);
+};
+
+// This Calls seedAll Function
+seedAll();
