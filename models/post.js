6e55d7fa0b8bd8e7bee5df584586104e05c8610c
Gabe Harvey <harvey7@gmail.com>
2024-03-09 12:06:26 -0600

    Established Post Model in models/post.js

--- models/post.js
+++ models/post.js
@@ -1,3 +1,44 @@
 // Imports Required for Post Model
 const sequelize = require("../config/connection");
 const { Model, DataTypes } = require("sequelize");
+
+// This Sets up Sequelize Model for Posts
+class Post extends Model {}
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post",
+    }
+);
+
+// This Exports Post Model
+module.exports = Post;
