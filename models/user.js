b9027502562141819c56b8ed18098824d0ec01ed
Gabe Harvey <harvey7@gmail.com>
2024-03-09 13:01:51 -0600

    Established Sequelized Model for User and Password Verification in models/user.js

--- models/user.js
+++ models/user.js
@@ -2,3 +2,62 @@
 const sequelize = require("../config/connection");
 const bcryptjs = require('bcryptjs');
 const { Model, DataTypes } = require("sequelize");
+
+// This Establishes Sequelize Model for User and Password Verification
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcryptjs.compareSync(loginPw, this.password);
+    }
+}
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    
+    // This Hashes User Password
+    {
+        hooks: {
+            async beforeCreate(newUser) {
+                newUser.password = await bcryptjs.hash(newUser.password, 10);
+                return newUser;
+            },
+            async beforeUpdate(updatedUser) {
+                updatedUser.password = await bcryptjs.hash(updatedUser.password, 10);
+                return updatedUser;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+    }
+);
+
+// This Exports User Model
+module.exports = User;
