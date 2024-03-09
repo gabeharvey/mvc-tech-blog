2c77ce761ea8aa5b92d63a3290a37ad3df241253
Gabe Harvey <harvey7@gmail.com>
2024-03-09 11:08:58 -0600

    Established Sequelize Comment Model in models/comment.js

--- models/comment.js
+++ models/comment.js
@@ -1,3 +1,49 @@
 // Imports Required for Comment Model
 const sequelize = require("../config/connection");
 const { Model, DataTypes } = require("sequelize");
+
+// This Sets up Sequelize Model for Comments
+class Comment extends Model {}
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment",
+    }
+);
+
+// This Exports the Comment Model
+module.exports = Comment;
