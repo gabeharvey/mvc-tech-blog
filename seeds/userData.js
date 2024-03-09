ab31477f3f5b847db48376333b44b061f563aa66
Gabe Harvey <harvey7@gmail.com>
2024-03-09 14:27:50 -0600

    Added User Data to Seed Database in seeds/userData.js

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,2 +1,32 @@
 // This Imports User Model
 const { User } = require("../models");
+
+// This Initially Seeds Database with User Data
+const userData = [
+    {
+        username: "blogger1",
+        email: "blogger1@codingrules.com",
+        password: "Password1",
+    },
+    {
+        username: "blogger2",
+        email: "blogger2@codingrules.com",
+        password: "Password2",
+    },
+    {
+        username: "blogger3",
+        email: "blogger3@codingrules.com",
+        password: "Password3",
+    },
+    {
+        username: "blogger4",
+        email: "blogger4@codingrules.com",
+        password: "Password4",
+    },
+];
+
+// This Inserts User Data into Database
+const seedUsers = () => User.bulkCreate(userData);
+
+// This Exports seedUsers Function
+module.exports = seedUsers;
