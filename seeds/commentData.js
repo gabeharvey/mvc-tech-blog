0635ff26fb3ea98a1426cf21c28f48846d3740ef
Gabe Harvey <harvey7@gmail.com>
2024-03-09 14:00:32 -0600

    Added Comment Data to Seed Database in seeds/commentData.js

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,2 +1,52 @@
 // This Imports Comment Model
 const { Comment } = require("../models");
+
+// This Initially Seeds Database with Comment Data
+const commentData = [
+    {
+        comment_text: "Coding is Awesome!",
+        user_id: 1,
+        post_id: 1,
+    },
+    {
+        comment_text: "Coding is Awesome!",
+        user_id: 2,
+        post_id: 1,
+    },
+    {
+        comment_text: "Coding is Awesome!",
+        user_id: 3,
+        post_id: 1,
+    },
+    {
+        comment_text: "Coding is Awesome!",
+        user_id: 4,
+        post_id: 1,
+    },
+    {
+        comment_text: "MVC Achieves Separation of Concerns!",
+        user_id: 1,
+        post_id: 2,
+    },
+    {
+        comment_text: "MVC Achieves Separation of Concerns!",
+        user_id: 2,
+        post_id: 2,
+    },
+    {
+        comment_text: "MVC Achieves Separation of Concerns!",
+        user_id: 3,
+        post_id: 2,
+    },
+    {
+        comment_text: "MVC Achieves Separation of Concerns!",
+        user_id: 4,
+        post_id: 2,
+    }
+];
+
+// This Inserts Comment Data into Database
+const seedComments = () => Comment.bulkCreate(commentData);
+
+// This Exports seedComments Function
+module.exports = seedComments;
