7f81f1dc6437fadfd7ed600dcd7dad276bdcf6c0
Gabe Harvey <harvey7@gmail.com>
2024-03-08 23:57:35 -0600

    Added Functions for Display Current Posts, Redirects, and Display New Post in controllers/home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,3 +37,47 @@ router.get("/post/:id", withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// This Function Displays Current User Posts
+router.get("/dashboard", withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: { user_id: req.session.user_id },
+            include: [{ model: User, attributes: ["username"] }],
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render("dashboard", {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// This Function Redirects User to Dashboard Page if Logged In, Displays Log In Form
+router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("/dashboard");
+        return;
+    }
+    res.render("login");
+});
+
+// This Function Redirects User to Dashboard Page if Logged In, Displays Sign Up Form
+router.get("/signup", (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("/dashboard");
+        return;
+    }
+    res.render("signup");
+});
+
+// This Function Displays New Post Page
+router.get("/newpost", (req, res) => {
+    if (req.session.logged_in) {
+        res.render("newpost");
+        return;
+    }
+    res.redirect("/login");
+});
