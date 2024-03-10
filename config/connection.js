940e6fd34de41ec1a3c036b920c376e78fa63d8c
Gabe Harvey <harvey7@gmail.com>
2024-03-09 21:50:05 -0600

    Added Connection to MySQL Database Information in config/connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,3 +3,17 @@ const Sequelize = require("sequelize");
 
 // This Imports .env Information
 require("dotenv").config();
+
+// This Allows Connection to MySQL Database
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize (process.env.JAWSDB_URL)
+    : new Sequelize (process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
+
+// This Exports Sequelize
+module.exports = sequelize;
